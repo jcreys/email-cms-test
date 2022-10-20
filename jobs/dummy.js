e86cf4a21166e5770fbe53b15f86aedb2313a367
Joshua Reyes <reyes.j15@gmail.com>
2022-10-20 12:24:28 -0400

    update

--- jobs/dummy.js
+++ jobs/dummy.js
@@ -7,6 +7,7 @@ const Sib = require("sib-api-v3-sdk");
 require("dotenv").config();
 const client = Sib.ApiClient.instance;
 const apiKey = client.authentications["api-key"];
+
 apiKey.apiKey = process.env.API_KEY;
 let apiInstance = new Sib.TransactionalEmailsApi();
 let sendSmtpEmail = new Sib.SendSmtpEmail();
